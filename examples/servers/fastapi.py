553aab0de179d9f8459cfbc6e8d56bd72649d6d2
sreenaths <somarajapuram@gmail.com>
2025-08-16 22:57:12 -0700

    [example] Adding scope to math_mcp

--- examples/servers/fastapi.py
+++ examples/servers/fastapi.py
@@ -3,7 +3,7 @@ import sys
 
 from fastapi import FastAPI, Request
 
-from .math_mcp_server import mcp
+from .math_mcp_server import math_mcp
 
 # Configure logging globally for the demo server
 logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
@@ -16,4 +16,4 @@ app = FastAPI()
 @app.post("/mcp")
 async def handle_mcp_request(request: Request):
     msg = await request.json()
-    return await mcp.handle(msg)
+    return await math_mcp.handle(msg)
