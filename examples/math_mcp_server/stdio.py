1a09f6e403fafd83e5c74133a05b5d9559941069
sreenaths <somarajapuram@gmail.com>
2025-08-18 21:52:19 -0700

    [examples] Simplifying stdio example with handles()

--- examples/math_mcp_server/stdio.py
+++ examples/math_mcp_server/stdio.py
@@ -1,16 +1,11 @@
-import json
 import logging
 import os
 import sys
-from asyncio.exceptions import CancelledError
 from io import TextIOWrapper
-from typing import Any
 
 import anyio
-import mcp.types as types
 
-from minimcp.server import MiniMCP, json_rpc
-from minimcp.server.utils import to_dict
+from minimcp.server import MiniMCP
 
 from .math_mcp import math_mcp
 
@@ -26,7 +21,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-async def stdio_server(mcp: MiniMCP):
+async def stdio_transport(mcp: MiniMCP):
     """
     Server transport for stdio: this communicates with an MCP client by reading
     from the current process' stdin and writing to stdout.
@@ -35,24 +30,14 @@ async def stdio_server(mcp: MiniMCP):
     stdin = anyio.wrap_file(TextIOWrapper(sys.stdin.buffer, encoding="utf-8"))
     stdout = anyio.wrap_file(TextIOWrapper(sys.stdout.buffer, encoding="utf-8"))
 
-    async def write_message(response: dict[str, Any]):
-        logger.info("Writing response message to stdio: %s", response)
-
-        msg_json = json.dumps(response)
-        await stdout.write(msg_json + "\n")
-        await stdout.flush()
-
     async def handle_message(line: str):
         logger.info("Handling incoming message: %s", line)
 
-        try:
-            message = json.loads(line)
-            response = await mcp.handle(message)
-        except json.JSONDecodeError as e:
-            response = to_dict(json_rpc.build_error_message(types.PARSE_ERROR, {}, e))
-
+        response = await mcp.handles(line)
         if response:
-            await write_message(response)
+            logger.info("Writing response message to stdio: %s", response)
+            await stdout.write(response + "\n")
+            await stdout.flush()
 
     logger.info("MiniMCP: Started stdio server, listening for messages...")
     try:
@@ -61,12 +46,12 @@ async def stdio_server(mcp: MiniMCP):
         async with anyio.create_task_group() as tg:
             async for line in stdin:
                 tg.start_soon(handle_message, line)
-    except (KeyboardInterrupt, CancelledError):
+    except (KeyboardInterrupt, anyio.get_cancelled_exc_class()):
         print("\nCtrl+C detected, exiting gracefully...")
 
 
 def main():
-    anyio.run(stdio_server, math_mcp)
+    anyio.run(stdio_transport, math_mcp)
 
 
 if __name__ == "__main__":
