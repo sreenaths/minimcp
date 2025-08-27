8970f10464c0b8155de14b36cdfbe9623b0da961
sreenaths <somarajapuram@gmail.com>
2025-08-27 16:39:38 -0700

    [utils] Pydantic util changes
    - Moved into minimcp/utils/model.py
    - Added to_json to generate UTF-8 JSON string

--- src/minimcp/server/json_rpc.py
+++ src/minimcp/server/json_rpc.py
@@ -10,7 +10,7 @@ from mcp.types import (
     ServerResult,
 )
 
-from minimcp.server.utils import to_dict
+from minimcp.utils.model import to_dict
 
 # TODO: Remove once https://github.com/modelcontextprotocol/python-sdk/pull/1310 is merged
 JSON_RPC_VERSION = "2.0"
--- src/minimcp/utils/model.py
+++ src/minimcp/utils/model.py
@@ -5,3 +5,7 @@ from pydantic import BaseModel
 
 def to_dict(model: BaseModel) -> dict[str, Any]:
     return model.model_dump(by_alias=True, mode="json", exclude_none=True)
+
+
+def to_json(model: BaseModel) -> str:
+    return model.model_dump_json(by_alias=True, exclude_none=True)
