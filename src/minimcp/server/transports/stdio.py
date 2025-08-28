1468fd5e0a29dbd7dee017dfeeae28943ac47123
sreenaths <somarajapuram@gmail.com>
2025-08-27 17:04:05 -0700

    [example] Updating stdio to work with string messages
    - Removing tests for stdio temporarily, as it needs to be
      rewritten with string type

--- src/minimcp/server/transports/stdio.py
+++ src/minimcp/server/transports/stdio.py
@@ -1,16 +1,12 @@
-import json
 import logging
 import sys
 from io import TextIOWrapper
 
 import anyio
-import mcp.types as types
 
-from minimcp.server import json_rpc
 from minimcp.server.responder import Responder
 from minimcp.server.transports.types import TransportRequestHandler
 from minimcp.server.types import Message
-from minimcp.server.utils import to_dict
 
 logger = logging.getLogger(__name__)
 
@@ -34,24 +30,21 @@ async def stdio_transport(handler: TransportRequestHandler):
     stdout = anyio.wrap_file(TextIOWrapper(sys.stdout.buffer, encoding="utf-8", line_buffering=True))
 
     async def write_msg(response: Message | None):
-        if response is not None:
+        if response:
             logger.info("Writing response message to stdio: %s", response)
-            await stdout.write(json.dumps(response, ensure_ascii=False) + "\n")
+            await stdout.write(response + "\n")
             await stdout.flush()
 
     async def handle_message(line: str):
-        line = line.rstrip("\n")
-        logger.info("Handling incoming message: %s", line)
+        line = line.rstrip("\n").strip()
 
-        try:
-            message_dict = json.loads(line)
-        except json.JSONDecodeError as e:
-            response = to_dict(json_rpc.build_error_message(types.PARSE_ERROR, {}, e))
-        else:
-            responder = Responder(message_dict, write_msg)
-            response = await handler(message_dict, responder)
+        if line:
+            logger.info("Handling incoming message: %s", line)
 
-        await write_msg(response)
+            responder = Responder(line, write_msg)
+            response = await handler(line, responder)
+
+            await write_msg(response)
 
     async with anyio.create_task_group() as tg:
         async for line in stdin:
