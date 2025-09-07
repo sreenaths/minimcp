c3a2762198f4b87a62119c09b9bce890f04bef7e
sreenaths <somarajapuram@gmail.com>
2025-09-07 13:28:14 -0700

    [transport] Starlette: Adding application-level lifecycle management for StreamableHTTPTransport

--- src/minimcp/server/transports/starlette.py
+++ src/minimcp/server/transports/starlette.py
@@ -1,5 +1,9 @@
+from collections.abc import AsyncGenerator
+from contextlib import asynccontextmanager
+
 from anyio.streams.memory import MemoryObjectReceiveStream
 from sse_starlette.sse import EventSourceResponse
+from starlette.applications import Starlette
 from starlette.background import BackgroundTask
 from starlette.requests import Request
 from starlette.responses import Response
@@ -8,6 +12,7 @@ from minimcp.server.transports.http import HTTPRequestHandler, HTTPTransport
 from minimcp.server.transports.streamable_http import StreamableHTTPRequestHandler, StreamableHTTPTransport
 
 
+# --- HTTP Transport ---
 async def http_transport(handler: HTTPRequestHandler, request: Request) -> Response:
     msg = await request.body()
     msg_str = msg.decode("utf-8")
@@ -20,13 +25,29 @@ async def http_transport(handler: HTTPRequestHandler, request: Request) -> Respo
     )
 
 
+# --- Streamable HTTP Transport ---
+TRANSPORT_STATE_OBJ_KEY = "_streamable_http_transport"
+
+
+@asynccontextmanager
+async def streamable_http_lifespan(app: Starlette) -> AsyncGenerator[None, None]:
+    async with StreamableHTTPTransport() as transport:
+        setattr(app.state, TRANSPORT_STATE_OBJ_KEY, transport)
+        yield
+
+
 async def streamable_http_transport(handler: StreamableHTTPRequestHandler, request: Request) -> Response:
     msg = await request.body()
     msg_str = msg.decode("utf-8")
 
-    transport = StreamableHTTPTransport()
-    await transport.start()
-    close_transport = BackgroundTask(transport.aclose)
+    transport: StreamableHTTPTransport | None = getattr(request.state, TRANSPORT_STATE_OBJ_KEY, None)
+    close_transport = None
+    if transport is None:
+        # No application-level StreamableHTTPTransport found; create a new transport instance for this request.
+        transport = StreamableHTTPTransport()
+        await transport.start()
+        # Use a BackgroundTask to ensure the transport is properly closed after the response is served.
+        close_transport = BackgroundTask(transport.aclose)
 
     result = await transport.dispatch(handler, request.method, request.headers, msg_str)
 
